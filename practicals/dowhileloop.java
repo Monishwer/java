38a0a856968a1b80d79df576939d3d95065628ef
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-04-07 20:55:51 +0530

    Add files via upload

---
+++ practicals/dowhileloop.java
@@ -0,0 +1,20 @@
+class dowhileloop{
+
+public void show(){
+
+int a = 1;
+do{
+System.out.println("the value is "+a);
+a++;
+
+}
+while(a > 5);
+}
+
+public static void main(String args[]){
+dowhileloop w = new dowhileloop();
+w.show();
+
+}
+
+}
