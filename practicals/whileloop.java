75b0cb9349b9d8f42d5ce6f09b6c25c95418d115
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-04-07 20:53:56 +0530

    Add files via upload

---
+++ practicals/whileloop.java
@@ -0,0 +1,18 @@
+class whileloop{
+
+public void show(){
+
+int i = 0;
+while(i < 5){
+System.out.println("hello the value is: "+i);
+i++;
+}
+}
+
+public static void main(String args[]){
+whileloop w = new whileloop();
+w.show();
+
+}
+
+}
