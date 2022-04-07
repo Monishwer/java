06524aa7508574f9975cf6ee94b5c421b00bada9
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-04-07 20:49:24 +0530

    Add files via upload

---
+++ practicals/forloop.java
@@ -0,0 +1,16 @@
+class forloop{
+
+public void display(){
+
+for(int i = 0; i < 11; i++){
+System.out.println("The value is "+i);
+}
+
+}
+
+public static void main(String args[]){
+forloop f = new forloop();
+f.display();
+
+}
+}
