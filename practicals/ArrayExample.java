55a18124780b58de4cf27b783e7bc45e82396cad
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-04-08 13:39:07 +0530

    Add files via upload

---
+++ practicals/ArrayExample.java
@@ -0,0 +1,18 @@
+class ArrayExample{
+
+public static void main(String args[]){
+
+String [] names = {"vijay","ajith","vikram","surya","dhanush","simbu"};
+//System.out.println(names[0]);
+for(int i = 0; i < names.length; i++){
+System.out.println(names[i]);
+}
+System.out.println();
+
+int [] phone = {243,453,253,876,987,945};
+for(int i = 0; i < phone.length; i++){
+System.out.println(phone[i]);
+}
+
+}
+}
