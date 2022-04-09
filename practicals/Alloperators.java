4c521e3a9516c62a0e52b3af29e5d428e03e09aa
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-04-09 09:10:51 +0530

    Add files via upload

---
+++ practicals/Alloperators.java
@@ -0,0 +1,16 @@
+class Alloperators{
+public static void main(String args[]){
+
+int a = 10;
+int b = 20;
+int result = a+b;
+
+System.out.println( a > 20 || a < 50);
+System.out.println( a > 20 && a < 50);
+System.out.println( a == b );
+System.out.println( a != b);
+System.out.println("the addition is "+ result);
+
+}
+
+}
