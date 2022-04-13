503a4e27e2bc7fbc22a11f2ba3379b6150a9c5e4
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-04-13 08:30:06 +0530

    Add files via upload

---
+++ practicals/arrforeachexp.java
@@ -0,0 +1,21 @@
+class arrforeachexp{
+
+
+public static void main(String args[]){
+
+//int [] number = {1,2,3,4,5};
+String [] names = {"vijay","ajith"};
+//for(int i = 0; i < names.length; i++){
+
+//System.out.println(names[i]);
+//}
+//for(int n : number){
+//System.out.println(n);
+//}
+for(String n : names){
+System.out.println(n);
+} 
+
+}
+
+}
