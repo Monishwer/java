c982232b02848e57c9b250340b4f9ea6450d8ddb
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-04-26 13:45:20 +0530

    Add files via upload

---
+++ assignment/Companyinfo.java
@@ -0,0 +1,85 @@
+package com;
+
+class employee{
+	
+	long employeeId;
+	String employeeName;
+	String employeeAddress;
+	long employeePhone;
+	double basicSalary;
+	double specialAllowance = 250.80;
+	double Hra = 1000.50;
+	double transportAllowance;
+	
+	
+	
+	
+	void employee(long id,String name,String address,long phone){
+		
+		employeeId = id;
+		employeeName = name;
+		employeeAddress = address;
+		employeePhone = phone;
+	}
+	
+	void calculatesalary() {
+		
+		double salary;
+		salary = basicSalary + (basicSalary * specialAllowance/100) + (basicSalary*Hra/100);
+		System.out.println("salary is "+salary);
+	}
+	public void calculateTransportAllowance() {
+		
+		transportAllowance = (10*basicSalary)/100;
+		System.out.println("transportAllowance is : "+transportAllowance);
+	}
+}
+
+class manager extends employee{
+	
+
+	manager(long id,String name,String address,long phone,double salary) {
+		employeeId = id;
+		employeeName = name;
+		employeeAddress = address;
+		employeePhone = phone;
+		basicSalary = salary;
+	}
+	public void calculateTransportAllowance() {
+		transportAllowance = 15*basicSalary/100;
+		System.out.println("transportAllowance of manager : "+transportAllowance);
+	}
+	
+}
+class trainee extends employee{
+	
+	public trainee(int id, String name, String address, long phone, double salary) {
+		// TODO Auto-generated constructor stub
+		employeeId = id;
+		employeeName = name;
+		employeeAddress = address;
+		employeePhone = phone;
+		basicSalary = salary;
+	}
+	
+
+	
+	
+}
+
+
+public class Companyinfo {
+
+	public static void main(String[] args) {
+		manager m = new manager(126534,"peter","chennai india",237844, 65000.0);
+		m.calculatesalary();
+		trainee t = new trainee(29846,"Jack","Mumbai India",442085,45000);
+		t.calculatesalary();
+		m.calculateTransportAllowance();
+		t.calculateTransportAllowance();
+		
+		
+
+	}
+
+}
