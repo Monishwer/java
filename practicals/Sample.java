9551e9bbf56eeef95f2b440c1287a5b548e97714
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-05 19:25:02 +0530

    Add files via upload

---
+++ practicals/Sample.java
@@ -0,0 +1,35 @@
+package thread;
+
+public class Sample {
+//void print(int num) { //nonsync method
+	synchronized void print(int num) {//sync method
+	for(int i = 1; i<= 5; i++) {
+		System.out.println(num * i);
+		try {
+			Thread.sleep(500);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+}
+}
+class Thread1 extends Thread{
+	Sample t;
+	public Thread1(Sample t) {
+		this.t = t;
+	}
+	public void run() {
+		t.print(3);
+	}
+}
+class Thread2 extends Thread{
+	Sample t;
+	public Thread2(Sample t) {
+		this.t = t;
+	}
+	public void run() {
+		t.print(5);
+	}
+}
