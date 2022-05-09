d7ed81e17a1875c796964d1302b2b62475068a24
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-09 13:48:45 +0530

    Add files via upload

---
+++ practicals/ObjectInputExample.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class ObjectInputExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			ObjectInputStream oi = new ObjectInputStream(new FileInputStream("E://ab.txt"));
+			Student s = (Student) oi.readObject();
+			
+			System.out.println("id is : "+s.id + " name is : "+s.sname +" fees is : "+s.fees);
+			oi.close();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
