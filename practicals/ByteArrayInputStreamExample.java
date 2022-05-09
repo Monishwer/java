ae9509287d609b6597634ef7fa93f7bc54f12859
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-09 12:28:02 +0530

    Add files via upload

---
+++ practicals/ByteArrayInputStreamExample.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.io.ByteArrayInputStream;
+
+public class ByteArrayInputStreamExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte [] arr = {68, 37, 35, 70, 39};
+		ByteArrayInputStream bin  = new ByteArrayInputStream(arr);
+		
+		int i = 0;
+		while( (i = bin.read()) != -1) {
+			char ch = (char)i;
+			System.out.println("ASCII character "+i + " value is "+ch);
+		}
+
+	}
+
+}
