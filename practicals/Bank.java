ec1925f885611ee60a3101e4f1ce0a6758080d4c
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-13 08:51:31 +0530

    Add files via upload

---
+++ practicals/Bank.java
@@ -0,0 +1,17 @@
+package collections;
+
+public class Bank {
+	int accountNo;
+	String customerName;
+	double balance;
+	int customerAge;
+	
+	public Bank(int accountNo, String customerName, double balance, int customerAge) {
+		super();
+		this.accountNo = accountNo;
+		this.customerName = customerName;
+		this.balance = balance;
+		this.customerAge = customerAge;
+	}
+
+}
