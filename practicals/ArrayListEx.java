f9918153852b122c34e444ca54d1785c17f1b54d
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-13 08:50:20 +0530

    Add files via upload

---
+++ practicals/ArrayListEx.java
@@ -0,0 +1,46 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//creating arrayList
+		List<String> al = new ArrayList<String>();
+		System.out.println("arrayList is : "+al.isEmpty());
+		//adding object to arrayList
+		al.add("rose");
+		al.add("sunflower");
+		al.add("rose"); //duplicate
+		al.add(0, "lily");
+		al.add(2, "daisy");
+		al.add("marigold");
+		al.add(6, "tulip");
+		al.add("jasmine");
+		al.add("orchid");
+		al.add("lotus");
+		al.add(5,"lavender");
+		
+		//printing arrayList object
+		System.out.println(al);
+		System.out.println("size of arrayList : "+ al.size());
+		
+		//removing element at index 5
+		al.remove(5);
+		
+		//printing arrayList after remove
+		System.out.println(al);
+		System.out.println("size of arrayList : "+ al.size());
+		
+		al.add("lavendor");
+		al.add("lotus");
+		System.out.println(al);
+		System.out.println("size of arrayList : "+ al.size());
+		System.out.println("element of index value is : "+ al.get(6));
+			
+
+	}
+
+}
