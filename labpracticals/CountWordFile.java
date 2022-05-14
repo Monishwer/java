2758cce89e0867c1a06b03af5e222f5ba41cd9e5
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-14 11:19:54 +0530

    Add files via upload

---
+++ labpracticals/CountWordFile.java
@@ -0,0 +1,23 @@
+package Assignments;
+import java.io.BufferedReader;  
+import java.io.FileReader; 
+
+public class CountWordFile {
+	
+	    public static void main(String[] args) throws Exception
+	     {  
+	        String line;  
+	        int count = 0; 
+	        FileReader file = new FileReader("E:\\test.txt");  
+	        BufferedReader br = new BufferedReader(file);  
+	        while((line = br.readLine()) != null)
+	        {  
+	            String words[] = line.split(" ");  
+	            count = count + words.length;  
+	        } 
+	        System.out.println("Number of words present in given file: " + count);  
+	        br.close();  
+	    }  
+}
+ 
+  
