0949efc480e0c0ec9bc2ce92a598f53e589ca12d
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-19 19:40:50 +0530

    Add files via upload

---
+++ practicals/Book.java
@@ -0,0 +1,41 @@
+package collections;
+
+public class Book {
+	
+	String bookName;
+	String bookAuthor;
+	int bookPrice;
+	public Book(String bookName, String bookAuthor, int bookPrice) {
+		super();
+		this.bookName = bookName;
+		this.bookAuthor = bookAuthor;
+		this.bookPrice = bookPrice;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+	public int getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	
+	
+	
+
+	
+	
+	
+	
+
+}
