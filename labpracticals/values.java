8aa3990a75e845a4e96cd2a48c3a6a11302973f9
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-19 19:59:22 +0530

    Add files via upload

---
+++ labpracticals/values.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class values {
+	
+	String personName;
+	int salary;
+	public values(String personName, int salary) {
+		super();
+		this.personName = personName;
+		this.salary = salary;
+	}
+	
+	
+	
+
+}
