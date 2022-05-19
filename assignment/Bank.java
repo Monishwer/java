57371084bcbbde2dc43eff82aaf1ce03de5ab618
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-19 21:49:36 +0530

    Add files via upload

---
+++ assignment/Bank.java
@@ -0,0 +1,17 @@
+package Assignments;
+
+public class Bank {
+	long accountNo;
+	String customerName;
+	double balance;
+	int customerAge;
+	public Bank(long accountNo, String customerName, double balance, int customerAge) {
+		super();
+		this.accountNo = accountNo;
+		this.customerName = customerName;
+		this.balance = balance;
+		this.customerAge = customerAge;
+	}
+	
+	
+}
