62b94e15641f3419b3abd336972d030a1e264324
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-24 12:25:20 +0530

    Add files via upload

---
+++ assignment/ConvertStrLowerCase.java
@@ -0,0 +1,13 @@
+package Assignment2;
+
+public class ConvertStrLowerCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Anudip Foundation";
+		System.out.println("Original String : "+name);
+		System.out.println("String in lowercase : "+name.toLowerCase());
+	}
+
+}
