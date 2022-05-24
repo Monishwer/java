d709ce420d0f8350773449b6aff6eb600043ff08
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-24 12:25:49 +0530

    Add files via upload

---
+++ assignment/TrimString.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+public class TrimString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "    Anudip Foundation";
+		System.out.println("Original String : "+name);
+		System.out.println("New String : "+name.trim());
+	}
+
+}
