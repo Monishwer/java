ef37fca619cffd001948f87830a71a5b6a88a1bf
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-24 12:24:52 +0530

    Add files via upload

---
+++ assignment/SubstrOfString.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+public class SubstrOfString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "hello universe";
+		System.out.println("old ="+name);
+		System.out.println("new ="+name.substring(2,11));
+	}
+
+}
