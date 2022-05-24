c9d5b0967066e4e930ae754459b1275961c856f4
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-24 12:22:51 +0530

    Add files via upload

---
+++ assignment/CheckingTwoString.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+public class CheckingTwoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "Hello World";
+		String b = "Hello World";
+		boolean Result = a.equals(b);
+		System.out.println(Result);
+		
+	}
+
+}
