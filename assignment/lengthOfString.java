9adfe72c47892c42a4030462756abf76889be3d1
Monishwer <91957744+Monishwer@users.noreply.github.com>
2022-05-24 12:24:27 +0530

    Add files via upload

---
+++ assignment/lengthOfString.java
@@ -0,0 +1,11 @@
+package Assignment2;
+
+public class lengthOfString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "hello welcome";
+		System.out.println("the length of" +" "+ sentence+" is "+sentence.length());
+	}
+
+}
